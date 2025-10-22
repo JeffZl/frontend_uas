c246d9c19329129ebe04458b674de8ad34f6e7de
535240092_ANDREAN <andrean.fuardi88@gmail.com>
2025-10-22 13:42:09 +0700

    Add files via upload
    Update profile page

--- app/(routes)/profile/page.tsx
+++ app/(routes)/profile/page.tsx
@@ -1,6 +1,26 @@
+import React from "react"
+import ProfileHeader from "@/components/Profile/ProfileHeader"
+import UserBio from "@/components/Profile/UserBio"
+import ProfileTabs from "@/components/Profile/ProfileTabs"
+
 const page = () => {
     return (
-        <div>profile</div>
+        <div>
+           
+            <ProfileHeader
+                name="Jane Doe"
+            />
+            <UserBio
+                name="Jane Doe"
+                handle="bomb_gdoe"
+                bio="Just a regular Doe exploring the world." 
+                birthDate="January 1, 1990"
+                joinDate="March 2020"
+                followingCount={150}
+                followersCount={200}
+            />
+            <ProfileTabs />
+        </div>
     )
 }
 export default page
