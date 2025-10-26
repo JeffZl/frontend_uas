8e525c9dea10fe336a85853f143cef57338b9702
bananand <andrean.fuardi88@gmail.com>
2025-10-26 16:43:02 +0700

    Update profile page: profile picture

--- app/(routes)/profile/page.tsx
+++ app/(routes)/profile/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image"
 
 const page = () => {
     return (
-        <div>
+        <div className="profile-main-content">
            
             <ProfileHeader
                 name="Jane Doe"
@@ -16,23 +16,37 @@ const page = () => {
             <Image 
                 src="/icons/banner.jpg"
                 alt="Banner Image"
-                width={745}
-                height={250}
+                width={600}
+                height={200}
                 className="profile-banner-image"
             />
 
-            <EditProfileButton />
+            <div className="profile-details-section">
+                <div>
+                    <Image 
+                        src="https://robohash.org/magnamomnisdolorem.png?size=50x50&set=set1"
+                        alt="Profile Picture"
+                        width={373}
+                        height={120}
+                        className="profile-picture"
+                    />            
+                </div>
+                
+                <div className="profile-control">
+                    <EditProfileButton />
+                </div>
 
-            <UserBio
-                name="Jane Doe"
-                handle="bomb_gdoe"
-                bio="Just a regular Doe exploring the world." 
-                birthDate="January 1, 1990"
-                joinDate="March 2020"
-                followingCount={150}
-                followersCount={200}
-            />
-            <ProfileTabs />
+                <UserBio
+                    name="Jane Doe"
+                    handle="bomb_gdoe"
+                    bio="Just a regular Doe exploring the world." 
+                    birthDate="January 1, 1990"
+                    joinDate="March 2020"
+                    followingCount={150}
+                    followersCount={200}
+                />
+                <ProfileTabs />
+            </div>
         </div>
     )
 }
--- components/Profile/UserBio.tsx
+++ components/Profile/UserBio.tsx
@@ -14,7 +14,7 @@ export default function UserBio ({
     name, handle, bio, birthDate, joinDate, followingCount, followersCount
 }: UserBioElements) {
     return (
-        <div className="bio-details">
+        <div className="bio-details mt-2 px-4.5">
             <h1 className="display-name">{name}</h1>
             <p className="user-handle">@{handle}</p>
 
